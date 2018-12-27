69ea0c75faf3c5f344bd346fc4e5a42f21c4c5d4
Igor Fastroni <igor.fastroni@gmail.com>
2018-12-27 10:18:23 -0200

    refactor in news, create NewsService, NewsRepository

--- Controllers/NewsController.js
+++ Controllers/NewsController.js
@@ -1,24 +1,10 @@
-News = require('../Models/News');
-let date = new Date();
+const NewsService = require('../Services/NewsService');
+const newsService = new NewsService();
 
 exports.create = (req, res) => {
-    var news = new News();
-    news.title = req.body.title;
-	news.description = req.body.description;
-	news.published = req.body.published;
-	news.qrcode = req.body.qrcode;
+	res.json(newsService.create(req, res));
+}
 
-    news.save((error, person) => {
-		res.json({
-			status: "success",
-			message: 'New news created with success!',
-			data: person,
-        });
-	});
-};
-
-exports.news = (req, res) => {
-	News.find({}, (error, news) => {
-		res.json(news);
-	});
-};
+exports.news = async (req, res) => {
+	res.json(await newsService.findNews());
+}
---
+++ Repositories/NewsRepository.js
@@ -0,0 +1,18 @@
+const News = require('../Models/News')
+class NewsRepository {
+    saveNews(news) {
+        return news.save((error, news) => {
+            return {
+                status: "success",
+                message: 'New news has created!',
+                data: news,
+            };
+        });
+    }
+    
+    findNews() {
+        return News.find();
+    }
+}
+
+module.exports = NewsRepository;
